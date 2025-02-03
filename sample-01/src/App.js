b093f86f6802017a5d1a0bd99c51aed22debb950
leoduville <71992403+leoduville@users.noreply.github.com>
2025-02-03 20:53:10 -0300

    a

--- sample-01/src/App.js
+++ sample-01/src/App.js
@@ -5,7 +5,6 @@ import { Container } from "reactstrap";
 import Loading from "./components/Loading";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import LoginButton from "./components/LoginButton";
 import Home from "./views/Home";
 import Profile from "./views/Profile";
 import ExternalApi from "./views/ExternalApi";
