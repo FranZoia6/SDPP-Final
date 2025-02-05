2fd06e81fced168dcb7df8a6b1757d13c7003b38
FranZoia63 <franco.zoia@juncofilms.org>
2025-02-05 19:16:50 -0300

    feat: frontend

---
+++ frontend/src/component/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LeftSide from "./LeftSide";
+
+const MainLayout = () => {
+  return (
+    <div className="mainLayout">
+      <LeftSide />
+    </div>
+  );
+};
+
+export default MainLayout;
